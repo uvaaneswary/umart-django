963e6c01e088987dfdfeff5bf5b481a57fe5391d
uvaaneswary <86975022+uvaaneswary@users.noreply.github.com>
2022-10-16 03:18:33 +0800

    Update settings.py

--- umart/settings.py
+++ umart/settings.py
@@ -145,6 +145,6 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'uvaarajen26@gmail.com'
-EMAIL_HOST_PASSWORD = 'bnic rkgd oyyj lrzz'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True 
 
