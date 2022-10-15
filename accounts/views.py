e254b6386f057f5a2b66d45312f1ac5ee76e7d2d
Uvaaneswary <uvaaneswaryrajen26@gmail.com>
2022-10-16 03:09:25 +0800

    django authentication

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,162 @@
-from django.shortcuts import render
+from email.policy import default
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from .models import Account   # after import is (class name)
+from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+
+#verification email
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+
+from django.contrib.sessions.models import Session
+Session.objects.all().delete()
 
 # Create your views here.
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+    
+        if form.is_valid():  #is valid used for cleaning method
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            phone_number = form.cleaned_data['phone_number']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            username = email.split("@")[0]
+            user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)  #creating user object
+            user.phone_number = phone_number
+            user.save()
+            
+            # USER ACTIVATION
+            current_site = get_current_site(request)
+            mail_subject = 'Please activate your account'
+            message = render_to_string('accounts/account_verification_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),  #user.pk means userid (primary key). Encoding the user id can make others not to see the primary key. So when we deactivating the account, we decode it
+                'token': default_token_generator.make_token(user),  #default........is actually the library. make_token is a function. default token generator will generate the token of the particular user
+            })
+            #sending email
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+            #messages.success(request, 'Registration successful.')
+            return redirect('/accounts/login/?command=verification&email='+email)
+            
+    else:
+        form = RegistrationForm()  #get method and the registration form should be rendered
+    context = {
+        'form' : form
+    }
+    return render(request, 'accounts/register.html', context)
+
+def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        
+        user = auth.authenticate(email=email, password = password)
+        
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are now logged in.')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid login credentials')
+            return redirect('login')
+    return render(request, 'accounts/login.html')
+
+@login_required(login_url = 'login')
+def logout(request):
+    auth.logout(request)
+    messages.success(request, 'You are logged out.')
+    return redirect('login')
+
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()  #decode the uidb64 and store it in uid. So, uid will give the primary key of the user
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+    
+    #check the token
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'Congratulations! Your account is activated.')
+        return redirect('login')
+    else:
+        messages.error(request, 'Invalid activation link')
+        return redirect('register')
+    
+@login_required(login_url = 'login')
+def dashboard (request):
+    return render(request, 'accounts/dashboard.html')
+
+def forgotPassword(request):
+    if request.method == 'POST':
+        email=request.POST['email']
+        if Account.objects.filter(email=email).exists():
+            user = Account.objects.get(email__exact=email)
+            
+            # Reset password email
+            current_site = get_current_site(request)
+            mail_subject = 'Reset Your Password'
+            message = render_to_string('accounts/reset_password_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),  #user.pk means userid (primary key). Encoding the user id can make others not to see the primary key. So when we deactivating the account, we decode it
+                'token': default_token_generator.make_token(user),  #default........is actually the library. make_token is a function. default token generator will generate the token of the particular user
+            })
+            #sending email
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+            
+            messages.success(request, 'Password reset email has been sent to your email address.')
+            return redirect('login')
+        else:
+            messages.error(request, 'Account does not exist!')
+            return redirect('forgotPassword')
+    return render(request, 'accounts/forgotPassword.html')
+
+def resetpassword_validate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()  #decode the uidb64 and store it in uid. So, uid will give the primary key of the user
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+        
+    if user is not None and default_token_generator.check_token(user, token):  #checking request is important to know whether the request is secure request or not
+        request.session['uid'] = uid #save uid inside here so that we can access the session later when we reseting the password
+        messages.success(request, 'Please reset your password!')
+        return redirect('resetPassword')
+    else:
+        messages.error(request, 'This link has been expired!')
+        return redirect('login')
+    
+def resetPassword(request):
+    if request.method == 'POST':
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        
+        if password == confirm_password:
+            uid = request.session.get('uid')
+            user = Account.objects.get(pk=uid)
+            user.set_password(password)
+            user.save()
+            messages.success(request, 'Password reset successful')
+            return redirect('login')
+        else:
+            messages.error(request, 'Password do not match!')
+            return redirect('resetPassword')
+    else:
+        return render(request, 'accounts/resetPassword.html')
+
+
--- umart/settings.py
+++ umart/settings.py
@@ -136,3 +136,15 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
 
+from django.contrib.messages import constants as messages     #Got from django framework
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',  #ERROR is the name of the bootstrap name
+}
+
+# SMTP Configure (email configuration)
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'uvaarajen26@gmail.com'
+EMAIL_HOST_PASSWORD = 'bnic rkgd oyyj lrzz'
+EMAIL_USE_TLS = True 
+
